0fffedadd8f1eacc13e34e6a60f007f6d06f0bfc
Nick Haglind <nhaglind@gmail.com>
2017-11-20 15:59:38 -0600

    wow big clean up

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    
+
     <p>{{userAgent}}</p>
-    
-    <img v-bind:src="browser"><img>
-    <img v-bind:src="device"></img>
+    <img v-for="image in images" :src="image">
+
   </div>
 </template>
 
@@ -14,35 +13,33 @@ export default {
   data () {
     return {
       userAgent: '',
-      browser: '',
-      device: ''
+      images: ''
     }
   },
   created: function () {
     this.displayUA();
-    this.browserType();
+    this.imageGrab();
   },
   methods: {
     displayUA: function() {
       this.userAgent = navigator.userAgent
     },
-    browserType: function() {
+    imageGrab: function() {
       let responseUA = this.userAgent = navigator.userAgent;
+      let images = [];
+      this.images = images;
+
       if (responseUA.includes("Chrome")) {
-        console.log('Chrome');
-        this.browser = 'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/20170911_Google_Chrome_logo_vector_download.png';
+        images.push('https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/20170911_Google_Chrome_logo_vector_download.png');
       }
       if (responseUA.includes("Firefox")) {
-        console.log('Firefox');
-        this.browser = 'https://www.underconsideration.com/brandnew/archives/firefox_2017_logo.jpg';
+        images.push('https://www.underconsideration.com/brandnew/archives/firefox_2017_logo.jpg');
       }
       if (responseUA.includes("Mac")) {
-        console.log('Mac');
-        this.device = 'https://www.logodesignlove.com/images/classic/apple-logo-rob-janoff-01.jpg';
+        images.push('https://www.logodesignlove.com/images/classic/apple-logo-rob-janoff-01.jpg');
       }
       if (responseUA.includes("Edge")) {
-        console.log('Edge');
-        this.browser = 'https://png.icons8.com/microsoft-edge/color/1600';
+        images.push('https://png.icons8.com/microsoft-edge/color/1600');
       }
     }
   }
